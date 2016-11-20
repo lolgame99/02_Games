32bc3ed5709a6e932417c7d2bb0bcdc1dc74d2b3
Benjamin Mayer <Benjamin@Benjamin-PC>
2016-11-20 22:19:30 +0100

    added walking animations

--- 02_Games/src/at/mab/mygames/RPGTest/Starter.java
+++ 02_Games/src/at/mab/mygames/RPGTest/Starter.java
@@ -34,7 +34,11 @@ public class Starter extends BasicGame{
 	public String direction;
 	private Rectangle hitbox;
 	private SpriteSheet move;
-	private Animation moveUp;
+	private Animation moveUpA;
+	private Animation moveDownA;
+	private Animation moveLeftA;
+	private Animation moveRightA;
+	private boolean giveDirection;
 	
 	public Starter() {
 		super("FirstGame");
@@ -43,25 +47,81 @@ public class Starter extends BasicGame{
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		this.map.render((int)this.posX, (int)this.posY, mapX, mapY, mapX+48, mapY+27);
-		g.drawString("Direction: "+this.direction, 10, 20);
+		g.drawString("Direction: "+this.direction, 10, 30);
 		
 		for (Rectangle bs : this.blocks) {
 			g.draw(bs);
 		}
 		
-		this.moveUp.draw();
 		g.draw(hitbox);
+		
+		if (this.direction=="u"&!gc.getInput().isKeyDown(Input.KEY_W)) {
+			this.move.draw(768, 418, 832, 482, 0, 0, 64, 64);
+		}
+		
+		if (this.direction=="d"&!gc.getInput().isKeyDown(Input.KEY_S)) {
+			this.move.draw(768, 418, 832, 482, 0, 128, 64, 192);
+		}
+		
+		if (this.direction=="l"&!gc.getInput().isKeyDown(Input.KEY_A)) {
+			this.move.draw(768, 418, 832, 482, 0, 64, 64, 128);
+		}
+		
+		if (this.direction=="r"&!gc.getInput().isKeyDown(Input.KEY_D)) {
+			this.move.draw(768, 418, 832, 482, 0, 192, 64, 256);
+		}
+		
+		if (this.direction=="u"&&gc.getInput().isKeyDown(Input.KEY_W)) {
+			this.moveUpA.draw(768, 418);
+		}
+		
+		if (this.direction=="d"&&gc.getInput().isKeyDown(Input.KEY_S)) {
+			this.moveDownA.draw(768, 418);
+		}
+		
+		if (this.direction=="l"&&gc.getInput().isKeyDown(Input.KEY_A)) {
+			this.moveLeftA.draw(768, 418);
+		}
+		
+		if (this.direction=="r"&&gc.getInput().isKeyDown(Input.KEY_D)) {
+			this.moveRightA.draw(768, 418);
+		}
+		
+		if (this.direction=="ur"&&gc.getInput().isKeyDown(Input.KEY_D)) {
+			this.moveRightA.draw(768, 418);
+		}
+		
+		if (this.direction=="dr"&&gc.getInput().isKeyDown(Input.KEY_D)) {
+			this.moveRightA.draw(768, 418);
+		}
+		
+		if (this.direction=="ul"&&gc.getInput().isKeyDown(Input.KEY_A)) {
+			this.moveLeftA.draw(768, 418);
+		}
+		
+		if (this.direction=="dl"&&gc.getInput().isKeyDown(Input.KEY_A)) {
+			this.moveLeftA.draw(768, 418);
+		}
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 		tileHitbox();
 		
-		this.hitbox = new Rectangle(800, 450, 40, 60);
+		this.direction = "d";
+		this.hitbox = new Rectangle(800, 450, 30, 46);
 		this.move = new SpriteSheet("testdata/player/player_walk.png", 64, 64);
-		this.moveUp = new Animation();
+		this.moveUpA = new Animation();
+		this.moveDownA = new Animation();
+		this.moveLeftA = new Animation();
+		this.moveRightA = new Animation();
+		this.hitbox.setCenterX(800);
+		this.hitbox.setCenterY(456);
 		for (int i=1;i<9;i++) {
-			moveUp.addFrame(move.getSprite(i,0), 150);
+			moveUpA.addFrame(move.getSprite(i,0), 150);
+			moveDownA.addFrame(move.getSprite(i,2), 150);
+			moveLeftA.addFrame(move.getSprite(i,1), 150);
+			moveRightA.addFrame(move.getSprite(i,3), 150);
 		}
 	}
 
@@ -132,21 +192,27 @@ public class Starter extends BasicGame{
 				switch (this.direction) {
 				case "u":
 					moveDown(delta);
+					moveDown(delta);
 					break;
 				
 				case "d":
+					moveUp(delta);
 					moveUp(delta);
 					break;
 				
 				case "l":
 					moveRight(delta);
+					moveRight(delta);
 					break;
 					
 				case "r":
 					moveLeft(delta);
+					moveLeft(delta);
 					break;
 				
 				case "ul":
+					moveDown(delta);
+					moveRight(delta);
 					moveDown(delta);
 					moveRight(delta);
 					break;
@@ -154,14 +220,20 @@ public class Starter extends BasicGame{
 				case "ur":
 					moveDown(delta);
 					moveLeft(delta);
+					moveDown(delta);
+					moveLeft(delta);
 					break;
 					
 				case "dl":
 					moveUp(delta);
 					moveRight(delta);
+					moveUp(delta);
+					moveRight(delta);
 					break;
 					
 				case "dr":
+					moveUp(delta);
+					moveLeft(delta);
 					moveUp(delta);
 					moveLeft(delta);
 					break;
